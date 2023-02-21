86beb2e503833a5ad072b006f6470f312fc2f6bc
CarlLamar <100596478+CarlLamar@users.noreply.github.com>
2023-02-21 12:53:27 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { of, Observable, map, from } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  //define number$ as an Observable
+  numbers$: Observable<number>;
+  constructor() {}
+}
+//create an array
+const numbers$ = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+//subscribing to the Observable
+numbers$.subscribe((value) => console.log(value));
+
+//manipulate data using map
+numbers$
+  .pipe(map((value) => value * 2))
+  .subscribe((value) => console.log(value));
+
+//create an observable with an array with an array observable of users objects
+const user = [
+  { id: 1, name: 'Alice', age: 25 },
+  { id: 2, name: 'Bob', age: 30 },
+  { id: 3, name: 'Charlie', age: 35 },
+  { id: 4, name: 'Dave', age: 40 },
+];
+
+//create the observable
+const user$ = from(user);
+user$.subscribe((user) => console.log(user));
+
+//subscribe to this observable
+
+user$
+  .pipe(
+    map((user) => ({
+      id: user.id,
+      name: user.name.toUpperCase(),
+      age: user.age * 2,
+    }))
+  )
+  .subscribe((user) => console.log(user));
